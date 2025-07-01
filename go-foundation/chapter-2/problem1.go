ff8b4ed7808a6d22295c1cdcfeb9e7101557d1fe
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-07-01 19:52:11 +0530

    chapter-2: problem 1 solved

--- go-foundation/chapter-2/problem1.go
+++ go-foundation/chapter-2/problem1.go
@@ -1,8 +1,24 @@
 package main
+
 import (
+	"crypto/sha256"
 	"fmt"
-	"crypto"
 )
+
 func main() {
 	fmt.Println("Hii crypto")
+
+	str := "MySecretPassword"
+
+	// Create new SHA-256 hasher
+	hash := sha256.New()
+
+	// Write the string to hasher (convert string to byte slice)
+	hash.Write([]byte(str))
+
+	// Get the final hash sum
+	hashed := hash.Sum(nil) // returns []byte
+
+	// Print the hash in hexadecimal format
+	fmt.Printf("SHA-256 hash: %x\n", hashed)
 }
